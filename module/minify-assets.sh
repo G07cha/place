71198831fbf66bf7472defbe5d1da3b2e2fc6066
G07cha <azizovkostya97@gmail.com>
2017-11-11 19:34:16 +0200

    Fix minify script
    Remove minified result before running closure compiler because it does not overwrites existing file

--- module/minify-assets.sh
+++ module/minify-assets.sh
@@ -1,9 +1,12 @@
 JAVA=$(whereis java)
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTPUT=static/js/main.min.js
 
 if [ -z "$JAVA" ]
 then
       echo "Can't find Java"
 fi
 
-cd $DIR && java -jar bin/closure-compiler.jar --js_output_file=static/js/main.min.js 'static/js/*.js'
+cd $DIR && \
+  rm $OUTPUT && \
+  $JAVA -jar bin/closure-compiler.jar --js_output_file=$OUTPUT 'static/js/*.js'
