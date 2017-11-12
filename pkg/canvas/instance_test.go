a0934c90a6efc6fe172dc4b087c9d13c627b7d5d
G07cha <azizovkostya97@gmail.com>
2017-11-12 09:55:40 +0200

    Add setPixel test

--- pkg/canvas/instance_test.go
+++ pkg/canvas/instance_test.go
@@ -1,17 +1,19 @@
 package canvas
 
 import (
+	"image/color"
 	"testing"
 )
 
+const (
+	height uint16 = 10
+	width  uint16 = 10
+)
+
 func TestCreateInstance(t *testing.T) {
-	var (
-		height uint16 = 10
-		width  uint16 = 10
-	)
 	i, err := NewCanvas(height, width)
 	if err != nil {
-		t.Errorf("Unexpected error when creating cluster %s", err)
+		t.Errorf("Unexpected error when creating canvas %s", err)
 	}
 
 	if i.data.Rect.Max.Y != int(height) {
@@ -20,5 +22,21 @@ func TestCreateInstance(t *testing.T) {
 
 	if i.data.Rect.Max.X != int(width) {
 		t.Errorf("Incorrect canvas width, expected %d but got %d", width, i.data.Rect.Max.X)
+	}
+}
+
+func TestSetPixel(t *testing.T) {
+	i, err := NewCanvas(height, width)
+	if err != nil {
+		t.Errorf("Unexpected error when creating canvas %s", err)
+	}
+
+	c := color.RGBA{1, 1, 1, 1}
+	i.Set(1, 1, c)
+	pixel := i.data.At(1, 1)
+	r, g, b, a := pixel.RGBA()
+	resultColor := color.RGBA{uint8(r), uint8(g), uint8(b), uint8(a)}
+	if resultColor != c {
+		t.Errorf("Incorrect color at target pixel, expected %v but got %v", c, resultColor)
 	}
 }
